d9582c2ed9d0d873da2f050df35f665c2476dc2b
DoThanhTin123 <tin.dt.63cntt@ntu.edu.vn>
2024-11-05 20:06:55 +0700

    Part 9 done

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -21,6 +21,14 @@
         return $data;
     }
 
+    function selectAll($table) {
+        $con = $GLOBALS['con'];
+        $res = mysqli_query($con, "SELECT * FROM $table");
+        if (!$res) {
+            die("Query failed: " . mysqli_error($con));
+        }
+        return $res;
+    }
     function select($sql,$values,$datatypes){
         $con =$GLOBALS['con'];
         if($stmt = mysqli_prepare($con,$sql)){
@@ -60,4 +68,44 @@
             die("Query cannot be prepared -Update");
         }
     }
+
+    function insert($sql,$values,$datatypes){
+        $con =$GLOBALS['con'];
+        if($stmt = mysqli_prepare($con,$sql)){
+            mysqli_stmt_bind_param($stmt, $datatypes,...$values);
+            if(mysqli_stmt_execute($stmt)){
+                $res = mysqli_stmt_affected_rows($stmt);
+                mysqli_stmt_close($stmt);
+                return $res;
+            }
+            else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be executed - Insert");
+            }
+
+        }
+        else{
+            die("Query cannot be prepared - Insert");
+        }
+    }
+
+    function delete($sql,$values,$datatypes){
+    $con =$GLOBALS['con'];
+    if($stmt = mysqli_prepare($con,$sql)){
+        mysqli_stmt_bind_param($stmt, $datatypes,...$values);
+        if(mysqli_stmt_execute($stmt)){
+            $res = mysqli_stmt_affected_rows($stmt);
+            mysqli_stmt_close($stmt);
+            return $res;
+        }
+        else{
+            mysqli_stmt_close($stmt);
+            die("Query cannot be executed -Delete");
+        }
+
+    }
+    else{
+        die("Query cannot be prepared -Delete");
+    }
+}
 ?>
