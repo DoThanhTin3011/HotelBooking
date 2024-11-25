e8bf8852c47191be86701bbb426d1f90a4afdfde
DoThanhTin123 <tin.dt.63cntt@ntu.edu.vn>
2024-11-25 20:57:26 +0700

    Update login_register.php

--- ajax/login_register.php
+++ ajax/login_register.php
@@ -2,7 +2,6 @@
 
   require('../admin/inc/db_config.php');
   require('../admin/inc/essentials.php');
-  require("../inc/sendgrid/sendgrid-php.php");
 
   date_default_timezone_set("Asia/Ho_Chi_Minh");
 
